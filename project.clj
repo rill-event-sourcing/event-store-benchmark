89c81f86d5baaa6faaf3c98f9a4b3a8a27f216fb
Joost Diepenmaat <joost@zeekat.nl>
2015-12-21 13:31:52 +0100

    Explicitly depend on postgres driver
    Current rill.event_store.psql does not have the postgresql jdbc driver
    in its dependencies, but it's loaded at runtime so you do need it.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.cli "0.3.3"]
+                 [postgresql "9.1-901.jdbc4"]
                  [rill-event-sourcing/rill.event_store.psql "0.2.1"]
                  [rill-event-sourcing/rill.event_store.memory "0.2.1"]]
   :main ^:skip-aot rill.event_store_benchmark
